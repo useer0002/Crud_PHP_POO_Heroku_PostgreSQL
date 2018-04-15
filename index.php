653f70cbefba5f4217769de76a59f38612c6e58c
useer0002 <38090566+useer0002@users.noreply.github.com>
2018-04-14 20:06:04 -0500

    Update index.php

--- index.php
+++ index.php
@@ -66,7 +66,7 @@
 
     <div class="ui small circular rotate left reveal image imgcenter">
         <img src="imagenes/heroku.png" class="visible content ">
-        <img src="imagenes/escanear0002.jpg" class="hidden content">
+        <img src="imagenes/escanear0001.jpg" class="hidden content">
     </div>
 <!--@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@-->
 
