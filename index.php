1f1f5a91fc7c3f9ab4dea538b6ed9281071ff30a
useer0002 <38090566+useer0002@users.noreply.github.com>
2018-04-14 19:59:42 -0500

    Update index.php
    changes photo

--- index.php
+++ index.php
@@ -66,7 +66,7 @@
 
     <div class="ui small circular rotate left reveal image imgcenter">
         <img src="imagenes/heroku.png" class="visible content ">
-        <img src="imagenes/1200px-Postgresql_elephant.svg.png" class="hidden content">
+        <img src="imagenes/escanear0002.jpg" class="hidden content">
     </div>
 <!--@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@-->
 
